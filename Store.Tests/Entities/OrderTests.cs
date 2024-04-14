b32c8e2726c88cb7d3be2fb7fdab195e781fa390
Ivan Clay <ivan.clay.moura@gmail.com>
2024-04-14 19:42:49 -0300

    Added order test class

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Entities;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void ShouldBeWaitedPaymentForNewOrder()
+    {
+        Assert.Fail();
+    }
+}
