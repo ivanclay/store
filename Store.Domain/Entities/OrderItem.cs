72680f5911ab55e3c4a2900f555da7d0a52f8fff
Ivan Clay <ivan.clay.moura@gmail.com>
2024-04-14 11:45:54 -0300

    Added flunt notifications to OrderItem

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,8 +1,17 @@
+using Flunt.Validations;
+
 namespace Store.Domain.Entities;
 public class OrderItem : Entity
 {
     public OrderItem(Product product, int quantity)
     {
+        AddNotifications(
+            new Contract<OrderItem>()
+            .Requires()
+            .IsNotNull(product, "Product", "Invalid product.")
+            .IsGreaterThan(quantity, 0, "Quantity", "Quantity should be greater then zero.")
+        );
+
         Product = product;
         Price = Product != null ? product.Price : 0;
         Quantity = quantity;
